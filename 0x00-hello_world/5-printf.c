ec4fae1c2bc697182f000e21631bf0cbb3e2f659
Ipfani Muleya <muleyaipfani22@gmail.com>
2023-03-16 16:48:49 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the main function is the starting point for program execution
+ *
+ * Return: gives 0 when there are no errors
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
