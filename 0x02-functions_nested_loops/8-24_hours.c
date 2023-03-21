dd280a6506a9450385652d049bb20ec304cb5c70
Ipfani Muleya <muleyaipfani22@gmail.com>
2023-03-21 14:38:49 -0700

    Prints time in 24-hour format

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer - Prints time in 24 hour format
+ */
+void jack_bauer(void)
+{
+	int i, j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
