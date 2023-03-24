cd42f6ede157b8ce0b7a99d44de4e1d2b6903621
Ipfani Muleya <muleyaipfani22@gmail.com>
2023-03-23 18:25:14 -0700

    Task 101 - prints integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,14 +5,16 @@
  */
 void print_number(int n)
 {
+	unsigned int i = n;
+
 	if (n < 0)
 	{
+		n *= -1;
+		i = n;
 		_putchar('-');
-		n = -n;
 	}
-	if (n >= 10)
-	{
-		print_number(n / 10);
-	}
-	_putchar(n % 10 + '0');
+	i /= 10;
+	if (i != 0)
+		print_number(i);
+	_putchar((unsigned int) n % 10 + '0');
 }
