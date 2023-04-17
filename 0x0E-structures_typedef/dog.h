1cbefb243688fee789090befc4698f6ded5aa855
Ipfani Muleya <muleyaipfani22@gmail.com>
2023-04-17 15:14:13 -0700

    Task 0 - A new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is the dog structure with name, age, and owner data
+ * @name: pointer to dog name string
+ * @age: dog age
+ * @owner: pointer to dog owner string
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
