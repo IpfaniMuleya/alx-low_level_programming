2ec99f6c442016e7c562c36b2998a04be1af9d17
Ipfani Muleya <muleyaipfani22@gmail.com>
2023-05-02 16:35:16 -0700

    Task 12 - update

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,16 +9,13 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *slower, *faster;
+	const listint_t *slower = head, *faster = head;
 	size_t counter = 0;
 
 	if (head == NULL)
 	{
 		exit(98);
 	}
-
-	slower = head;
-	faster = head;
 
 	while ((slower != NULL) && (faster != NULL) && (faster->next != NULL))
 	{
@@ -33,6 +30,16 @@ size_t print_listint_safe(const listint_t *head)
 			printf("-> [%p] %d\n", (void *)slower, slower->n);
 			counter++;
 			break;
+		}
+	}
+
+	if ((faster == NULL) || (faster->next == NULL))
+	{
+		while (slower != NULL)
+		{
+			printf("[%p] %d\n", (void *)slower, slower->n);
+			counter++;
+			slower = slower->next;
 		}
 	}
 
