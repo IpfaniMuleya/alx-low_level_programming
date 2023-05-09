2e264736e43472bf3c56e90c15745edcc8e15097
Ipfani Muleya <muleyaipfani22@gmail.com>
2023-05-08 17:25:11 -0700

    Task 1 - creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * create_file - creates afile and writes stuff to it
+ * @filename: name of the file to create
+ * @text_content: NULL terminated string to write to the file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int f_d;
+	ssize_t length;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	f_d = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (f_d == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		length = write(f_d, text_content, strlen(text_content));
+		if (length == -1)
+		{
+			close(f_d);
+			return (-1);
+		}
+	}
+
+	close(f_d);
+	return (1);
+}
